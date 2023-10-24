5e2d9f71df6a675da65ba7657e929030b7d56310
Ricardo (XenGi) Band <email@ricardo.band>
2023-10-24 15:59:04 +0200

    build nix package

--- flake.nix
+++ flake.nix
@@ -3,19 +3,37 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      #pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ gomod2nix.overlays.default ];
+      };
+      sanic = pkgs.buildGoApplication {
+        pname = "sanic";
+        version = "0.0.1";
+        src = ./.;
+        modules = ./gomod2nix.toml;
+      };
     in {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
+        buildInputs = with pkgs; [
           go
+          gomod2nix.packages.${system}.default
+        ];
+        packages = with pkgs; [
           mpd
           mpc-cli
+          sanic
         ];
       };
     }
   );
 }
-
