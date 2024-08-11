b343500d921a683818241000003763329405984a
Ricardo Band <email@ricardo.band>
2024-08-11 22:33:30 +0200

    remove dev machine to flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "sanic - chaos music control";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
     gomod2nix = {
       url = github:tweag/gomod2nix;
@@ -34,18 +34,6 @@
         packages = with pkgs; [
           mpd
           mpc-cli
-        ];
-      };
-      nixosConfigurations."sanic" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          {
-            environment.systemPackages = with pkgs; [
-              sanic
-            ];
-          }
-          "${nixpkgs}/nixos/modules/virtualisation/proxmox-lxc.nix"
-          ./configuration.nix
         ];
       };
     }
